58ea63059e018d726ef376de004dccad11c490d4
Julen Galindo <galindo.julen@uni.eus>
2026-01-13 14:11:49 +0100

    Botoiak sartuta

---
+++ assetssss erronka2/Assets/Scripts/ScriptsLobby/ShowKeyboard.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Experimental.UI;
+
+public class ShowKeyboard : MonoBehaviour
+{
+
+    private TMP_InputField inputField;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        inputField = GetComponent<TMP_InputField>();
+        inputField.onSelect.AddListener(x => OpenKeyboard());
+    }
+
+    // Update is called once per frame
+    public void OpenKeyboard()
+    {
+        NonNativeKeyboard.Instance.InputField = inputField;
+        NonNativeKeyboard.Instance.PresentKeyboard(inputField.text);
+    }
+}
